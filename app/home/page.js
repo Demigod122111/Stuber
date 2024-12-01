92b43b7831466121b3f33a936586f16f81707780
DOminic Haisley <haisleydominic@gmail.com>
2024-12-01 08:56:20 -0500

    home page started

--- app/home/page.js
+++ app/home/page.js
@@ -1,15 +1,24 @@
 "use client"
 
 import React, { useState } from 'react'
+import Image from 'next/Image';
+
 import "./styles.css"
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
+      <link rel="icon" type="image/x-icon" href="/icon.png"></link>
+      <nav>
+        <img src="/icon.png" className="icon"></img>
+      </nav>
       <div className="find-ride-menu">
-        <h1>Go anywhere with Stuber</h1>
+        <h1>Get home safely with</h1>
+        <img src="/logo.png" className="logo" alt="logo"></img>
+        <p>Find a driver, hop in, and go straight home.</p>
         <input type="text" placeholder="Enter location" className="location-input"></input>
         <input type="text" placeholder="Enter destination" className="destination-input"></input>
+        <button className="see-prices-button">See prices</button>
       </div>
     </div>
   )
